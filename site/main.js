7be4c261fd18b6591dc3c55ef971aa69c756d999
limzykenneth <limzy.kenneth@gmail.com>
2020-01-12 23:31:14 +0000

    Chart for benchmark

--- site/main.js
+++ site/main.js
@@ -25,4 +25,75 @@ fetch("./assets/results/benchmark.json").then((res) => res.json()).then((bench) 
 		results
 	});
 	document.querySelector("#benchmarks").innerHTML += html;
+
+	const backgroundColors = [
+		'rgba(255, 99, 132, 0.2)',
+		'rgba(54, 162, 235, 0.2)',
+		'rgba(255, 206, 86, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(153, 102, 255, 0.2)',
+		'rgba(255, 159, 64, 0.2)'
+	];
+	const borderColors = [
+		'rgba(255, 99, 132, 1)',
+		'rgba(54, 162, 235, 1)',
+		'rgba(255, 206, 86, 1)',
+		'rgba(75, 192, 192, 1)',
+		'rgba(153, 102, 255, 1)',
+		'rgba(255, 159, 64, 1)'
+	];
+
+	const labels = _.reduce(bench.results, (col, result) => {
+		if(!_.find(col, (el) => el === result.browser)){
+			col.push(result.browser);
+		}
+		return col;
+	}, []);
+
+	console.log(labels);
+	_.each(results, (result, key) => {
+		console.log(result);
+		const ctx = document.getElementById(key).getContext("2d");
+		let colorCount = 0;
+
+		const datasets = _.reduce(result, (col, r, k) => {
+			col.push({
+				label: k,
+				data: _.reduce(r, (c, r2) => {
+					const index = _.indexOf(labels, r2.browser)
+					c[index] = r2.opsPerSecond;
+					return c;
+				}, []),
+				backgroundColor: [
+					backgroundColors[colorCount],
+					backgroundColors[colorCount],
+				],
+				borderColor: [
+					borderColors[colorCount],
+					borderColors[colorCount],
+				],
+				borderWidth: 1
+			});
+
+			colorCount++;
+			return col;
+		}, []);
+
+		new Chart(ctx, {
+			type: "bar",
+			data: {
+				labels: labels,
+				datasets: datasets
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				}
+			}
+		});
+	});
 });
