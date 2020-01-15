2be2d1da09ffb6bfc49dd68351eb2417e3a0b163
limzykenneth <limzy.kenneth@gmail.com>
2020-01-15 13:49:34 +0000

    More realistic test for vectors

--- tests/vector.js
+++ tests/vector.js
@@ -1,19 +1,31 @@
 /* global createVector */
+/* eslint no-unused-vars: "off" */
 
 suite("createVector", () => {
 	benchmark("p5.createVector", () => {
-		createVector();
+		let v = createVector();
 	});
 
 	benchmark("p5.createVector 1D", () => {
-		createVector(1.2);
+		let v = createVector(1.2);
 	});
 
 	benchmark("p5.createVector 2D", () => {
-		createVector(1.2, 2.2);
+		let v = createVector(1.2, 2.2);
 	});
 
 	benchmark("p5.createVector 3D", () => {
-		createVector(1.2, 2.2, 3.2);
+		let v = createVector(1.2, 2.2, 3.2);
+	});
+});
+
+suite("Vector.toString", () => {
+	let v;
+	benchmark("p5.Vector.toString", () => {
+		v.toString();
+	}, {
+		setup(){
+			v = createVector(1.2, 2.2, 3.2);
+		}
 	});
 });
